1aa23e1ee139704019525017b4b37fe4ec44a382
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2019-03-29 16:59:17 +0100

    fixes

--- crawler2/index.js
+++ crawler2/index.js
@@ -3,12 +3,13 @@
 
 module.exports = function (pack) {
     //process.stdout.write("\nHello, World " + pack.content + '\n');
-    process.stdout.write(pack.name)
+    // process.stdout.write(pack.content)
+    // process.stdout.write('\n\n');
     return [
         $ok(pack),
-        $work([
-            worktype.Http({ method: 'GET' }),
-            worktype.Duktape({ script: './concert.js' })
-        ])
+        // $work($package("", "http://loppen.dk/concert/id"), [
+        //     worktype.Http({ method: 'GET' }),
+        //     worktype.Duktape({ script: './concert.js' })
+        // ])
     ]
 }
--- crawler2/src/worktypes/duktape/vm.rs
+++ crawler2/src/worktypes/duktape/vm.rs
@@ -1,24 +1,24 @@
-use duktape2::prelude::{Context as DukContext,Require, file_resolver, Compile, Environment, ContextCommonJS, FromDuktapeContext,JSFunction, Function, JSValue, JSObject, Object};
-use super::super::super::context::{Context};
+use duktape2::prelude::*;
+use super::super::super::context::{Context as CrawlContext};
 use super::super::super::error::CrawlResult;
 use vfs::physical::PhysicalFS;
 use conveyor_work::{package::Package};
 use super::super::super::work::WorkOutput;
-use conveyor::Result;
+use conveyor::{Result, ConveyorError};
 
 pub(crate) static REQUIRE_JS: &'static str = include_str!("./runtime.js");
 
 
 pub struct VM {
-    inner: DukContext,
-    ctx: Context,
+    inner: Context,
+    ctx: CrawlContext,
     pub(crate) script: String,
 }
 
 impl VM {
-    pub fn new<S: AsRef<str>>(mut ctx: Context, path:S) -> VM {
+    pub fn new<S: AsRef<str>>(mut ctx: CrawlContext, path:S) -> VM {
 
-        let duk = DukContext::new().unwrap();
+        let duk = Context::new().unwrap();
 
         Require::new()
         .env(Environment::from_env().unwrap())
@@ -55,7 +55,7 @@ impl VM {
         }
     }
 
-    pub fn ctx(&self) -> &Context {
+    pub fn ctx(&self) -> &CrawlContext {
         &self.ctx
     }
 
@@ -63,18 +63,147 @@ impl VM {
 
         let module = self.inner.require(&self.script).unwrap().push();
         let function: Function = self.inner.getp().unwrap();
-        let p = self.inner.get_global_string("Package").getp::<Function>().unwrap();
-        // p.set("name", &package.0).unwrap();
-        // p.set("content", &package.1).unwrap();
+        let p_ctor = self.inner.get_global_string("Package").getp::<Function>().unwrap();
+        let p = p_ctor.construct::<_, Object>((&package.0, package.1.as_slice())).unwrap();
+        let re = function.call::<_, Array>(p).unwrap();
+        let pack = parse(&re).unwrap();
 
-        let p = p.construct::<_, Object>((&package.0, &package.1)).unwrap();
+        // let pack = match deserialize_reference(&re) {
+        //     Err(e) => unimplemented!("could not do"),
+        //     Ok(s) => match s {
+        //         ValueOrBytes::Bytes(bs) => Package::new(package.0, bs),
+        //         ValueOrBytes::Value(v)  => {
+        //             println!("{}", serde_json::to_string_pretty(&v).unwrap());
+        //             Package::new(package.0, v)
+        //         },
+        //     }
+        // };
 
-        function.call::<_, Object>(p).unwrap().push();
+        Ok(pack)
 
-        println!("ctx {:?}",self.inner);
-
-        //println!("module: {}", module);
-
-        return Ok(vec![WorkOutput::Result(Ok(Package::new(package.0, package.1)))])
+        // return Ok(vec![WorkOutput::Result(Ok(pack))])
     }
+}
+
+
+fn parse(array: &Array) -> DukResult<Vec<WorkOutput<Package>>> {
+    let iter = array.iter();
+    for entry in iter {
+        let o: Object = entry.to()?;
+        let w = match o.get::<_, &str>("type")? {
+            "ok" => {
+                WorkOutput::Result(Ok(parse_package(&o.get::<_,Object>("package")?)?))
+            },
+            "then" => {
+                WorkOutput::Then(parse_package(&o.get::<_,Object>("package")?)?)
+            },
+            "err" => {
+                unimplemented!("Error {}", o)
+            }
+            _ => {
+                 unimplemented!("Error")
+            }
+        };
+    }
+
+    Ok(Vec::new())
+}
+
+fn parse_package(package: &Object) -> DukResult<Package> {
+    let name: &str = package.get("name")?;
+    let content: Reference = package.get("content")?;
+
+    let pack = match content.get_type() {
+        Type::String => Package::new(name, content.to::<&str>()?),
+        Type::Buffer => Package::new(name, content.to::<&[u8]>()?),
+        _ => return Err(DukError::new(DukErrorCode::Type, format!("invalid package content type: {:?}", content.get_type()))),
+    };
+
+    Ok(pack)
+}
+
+use serde_json::{Value,Number, Map};
+
+#[derive(Debug)]
+enum ValueOrBytes {
+    Value(Value),
+    Bytes(Vec<u8>),
+}
+
+fn decode(ctx: &Context, idx:Idx) -> DukResult<ValueOrBytes> {
+    let re: Reference = ctx.get(idx)?;
+    deserialize_reference(&re)
+}
+
+fn deserialize_reference2(value: &Reference) -> DukResult<Value> {
+    let val = match value.get_type() {
+        Type::Array => {
+            let re = value.to()?;
+            deserialize_array(&re)?
+        },
+        Type::String => Value::String(value.to()?),
+        Type::Null | Type::Undefined => Value::Null,
+        Type::Boolean => Value::Bool(value.to()?),
+        Type::Number => Value::Number(Number::from_f64(value.to()?).unwrap()),
+        Type::Object => {
+            let o:Object = value.to()?;
+            let iter = o.iter();
+            let mut out = Map::new();
+            for e in iter {
+                out.insert(e.0.to_string(), deserialize_reference2(&e.1)?);
+            }
+            Value::Object(out)
+        }
+        _ => {
+            unimplemented!("reference {:?}", value.get_type());
+        }
+    };
+
+    Ok(val)
+}
+
+fn deserialize_reference(value: &Reference) -> DukResult<ValueOrBytes> {
+    let val = match value.get_type() {
+        Type::Array => {
+            let re = value.to()?;
+            deserialize_array(&re)?
+        },
+        Type::String => Value::String(value.to()?),
+        Type::Null | Type::Undefined => Value::Null,
+        Type::Boolean => Value::Bool(value.to()?),
+        Type::Number => Value::Number(Number::from_f64(value.to()?).unwrap()),
+        Type::Buffer => {
+            let bs = value.to()?;
+            return Ok(ValueOrBytes::Bytes(bs));
+        }
+        Type::Object => {
+            let o:Object = value.to()?;
+            let iter = o.iter();
+            let mut out = Map::new();
+            for e in iter {
+                out.insert(e.0.to_string(), deserialize_reference2(&e.1)?);
+            }
+            Value::Object(out)
+        }
+        _ => {
+            unimplemented!("reference {:?}", value.get_type());
+        }
+    };
+
+    Ok(ValueOrBytes::Value(val))
+}
+
+fn deserialize_array(array: &Array) -> DukResult<Value> {
+
+    let mut out = Vec::with_capacity(array.len());
+    
+    for item in array.iter() {
+        out.push(match deserialize_reference(&item)? {
+            ValueOrBytes::Value(v) => v,
+            ValueOrBytes::Bytes(b) => return Err(DukError::new(DukErrorCode::Type, "could not be bytes"))
+        });
+    }
+
+    Ok(Value::Array(out))
+
 }
